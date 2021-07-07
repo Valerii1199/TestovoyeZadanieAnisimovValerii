1a633959cadc92cf7ebe6a160b7a024b1f6b4764
Valerii1199 <87093013+Valerii1199@users.noreply.github.com>
2021-07-07 22:46:37 +0300

    changes
    changes

--- Zadanie3_Anisimov_Valerii/Zadanie3_Anisimov_Valerii/Program.cs
+++ Zadanie3_Anisimov_Valerii/Zadanie3_Anisimov_Valerii/Program.cs
@@ -6,23 +6,22 @@ namespace Zadanie3_Anisimov_Valerii
     {
         static void Main(string[] args)
         {
-            // Задать с клавиатуры число элементов массива
+            
             int n;
             Console.WriteLine("Введите число элементов массива");
             n = Convert.ToInt16(Console.ReadLine());
-            // Объявим одномерный массив Data с n-элементами 
+            
             double[] Tara = new double[n];
             int i = 0;
             while (i < n)
             {
                 Console.WriteLine("Введите элемент массива");
-                /* Вводим элементы массива с клавиатуры 
-                * и заполняем ими массив */
+               
                 Tara[i] = double.Parse(Console.ReadLine());
                 Console.WriteLine();
                 i++;
             }
-            // Вывод содержимого массива
+            
             for (i = 0; i < n; i = i + 1)
                 Console.Write(Tara[i] + " ");
 
